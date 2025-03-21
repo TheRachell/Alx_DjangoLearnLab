6213496ece3016306f075d3fcf09af21467e8c46
rachael <rolukowade@gmail.com>
2025-03-21 11:51:42 +0100

    implemented role-based access control

--- LibraryProject/relationship_app/urls.py
+++ LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from .views import list_books, LibraryDetailView, register_view, login_view, logout_view
+from .views import admin_view, librarian_view, member_view
 
 urlpatterns = [
     path('books/', list_books, name = 'list_books'),
@@ -7,4 +8,7 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 ]
--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.views.generic import DetailView
 from .models import Book, Library
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
@@ -40,5 +41,21 @@ def logout_view(request):
     logout(request)
     return render(request, 'logout.html')
 
+def check_role(role):
+    def decorator(user):
+        return user.is_authenticated and user.userprofile.role == role
+    return decorator 
+
+@user_passes_test(check_role('Admin'))
+def admin_view(request):
+    return HttpResponse("Welcome to the Admin page!")
+
+@user_passes_test(check_role('Librarian'))
+def librarian_view(request):
+    return HttpResponse("Welcome to the Librarian page!")
+
+@user_passes_test(check_role('Member'))
+def member_view(request):
+    return HttpResponse("Welcome to the Member page!")    
     
 
